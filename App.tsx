25dfaef8743dc506e68e5dac7911ca3f403a72cc
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-10-06 10:20:36 +0300

    Init app and ui theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useState } from "react";
+
+import { ApplicationProvider, Button, Layout, Text } from "@ui-kitten/components";
+import * as eva from "@eva-design/eva";
+import theme from "./common/theme";
 
 export default function App() {
+  const [appTheme, setAppTheme] = useState("dark");
+
+  const handleChangeTheme = useCallback(() => {
+    setAppTheme((prev) => (prev === "dark" ? "light" : "dark"));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={appTheme === "dark" ? theme.dark : theme.light}>
+      <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text category="h1">HOME</Text>
+        <Button onPress={handleChangeTheme}>Toggle theme</Button>
+      </Layout>
+    </ApplicationProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
